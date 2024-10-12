9f7ccca382f99a1eab40198b5e1e735d3dacb17e
Raymond Zeng <shilei.zeng98@outlook.com>
2024-10-12 13:09:31 -0400

    Change connection of RZ to remote.

--- UnitTestLoginPage/Properties/Settings.Designer.cs
+++ UnitTestLoginPage/Properties/Settings.Designer.cs
@@ -26,12 +26,11 @@ namespace BookStoreLIB.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Lenovo\\Desktop\\Class" +
-            "es\\COMP4220\\Assignment\\RZCOMP4200A3\\xyBookstoreDB.mdf;Integrated Security=True;C" +
-            "onnect Timeout=30")]
-        public string RZConnections {
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=tfs.cs.uwindsor.ca;Initial Catalog=Agile2DB24;Persist Security Info=T" +
+            "rue;User ID=Agile2DB24;Password=Agile2DB24#;TrustServerCertificate=True")]
+        public string RZConnection {
             get {
-                return ((string)(this["RZConnections"]));
+                return ((string)(this["RZConnection"]));
             }
         }
         
@@ -40,9 +39,9 @@ namespace BookStoreLIB.Properties {
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=tfs.cs.uwindsor.ca;Initial Catalog=Agile2DB24;Persist Security Info=T" +
             "rue;User ID=Agile2DB24;Password=Agile2DB24#;TrustServerCertificate=True")]
-        public string RZConnection {
+        public string MSSQLConnection {
             get {
-                return ((string)(this["RZConnection"]));
+                return ((string)(this["MSSQLConnection"]));
             }
         }
     }
