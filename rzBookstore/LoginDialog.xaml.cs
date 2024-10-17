5e998a7cae937dee2c549e00b846576737ab3d49
Raymond Zeng <shilei.zeng98@outlook.com>
2024-10-17 19:51:34 -0400

    Created Signup textblock in Login Dialog and linked to Signup Dialog

--- rzBookstore/LoginDialog.xaml.cs
+++ rzBookstore/LoginDialog.xaml.cs
@@ -1,29 +1,28 @@
-﻿/* **********************************************************************************
- * For use by students taking 60-422 (Fall, 2014) to work on assignments and project.
- * Permission required material. Contact: xyuan@uwindsor.ca 
- * **********************************************************************************/
-
-using System.Windows;
-
-
+﻿using System.Windows;
 namespace BookStoreGUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class LoginDialog : Window
     {
         public LoginDialog()
         {
             InitializeComponent();
         }
+
         private void okButton_Click(object sender, RoutedEventArgs e)
         {
             this.DialogResult = true;
         }
+
         private void cancelButton_Click(object sender, RoutedEventArgs e)
         {
             this.DialogResult = false;
+        }
+
+        private void signupButton_Click(object sender, RoutedEventArgs e)
+        {
+            SignupDialog signupDialog = new SignupDialog();
+            this.Close(); 
+            signupDialog.ShowDialog(); 
         }
     }
 }
---
+++ rzBookstore/SignupDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace BookStoreGUI
+{
+    /// <summary>
+    /// Interaction logic for SignupDialog.xaml
+    /// </summary>
+    public partial class SignupDialog : Window
+    {
+        public SignupDialog()
+        {
+            InitializeComponent();
+        }
+
+        // Handle the Signup button click
+        private void signupOkButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void signupCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
