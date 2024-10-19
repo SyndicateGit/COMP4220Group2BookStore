2cc759d9d288cadb0b309e35a50692b98c188ae7
MoussaMJS <156938436+MoussaMJS@users.noreply.github.com>
2024-10-19 15:33:33 -0400

    Added Purchase History Button to main window.
    Updated DALBookCatalog to update the database when new orders are placed. Specifically Orders table and OrderItem reflect new orders.

---
+++ rzBookstore/PHDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using BookStoreLIB;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookStoreGUI
+{
+    /// <summary>
+    /// Interaction logic for PHDialog.xaml
+    /// </summary>
+    public partial class PHDialog : Window
+    {
+        public PHDialog(int userID)
+        {
+            InitializeComponent();
+
+            PurchaseHistory ph = new PurchaseHistory();
+            DataSet ds = ph.GetPurchaseHistory(userID);
+
+            // Check if data was retrieved
+            if (ds != null && ds.Tables["PurchaseHistory"].Rows.Count > 0)
+            {
+                // Bind the DataSet to the DataGrid
+                purchaseHistoryDataGrid.ItemsSource = ds.Tables["PurchaseHistory"].DefaultView;
+            }
+            else
+            {
+                MessageBox.Show("No purchase history found for this user.");
+            }
+        }
+
+        private void exitButton_Click(Object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
