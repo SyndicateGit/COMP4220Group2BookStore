5f8a88dd4f70bde124cf9311fe6054934d9af85d
MoussaMJS <156938436+MoussaMJS@users.noreply.github.com>
2024-10-19 15:29:57 -0400

    Add files via upload

---
+++ UnitTestLoginPage/PurchaseHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    public class PurchaseHistory
+    {
+        public DataSet GetPurchaseHistory(int userID)
+        {
+            DALPurchaseHistory purchaseHistory = new DALPurchaseHistory();
+            return purchaseHistory.GetPhInfo(userID);
+        }
+    }
+}
