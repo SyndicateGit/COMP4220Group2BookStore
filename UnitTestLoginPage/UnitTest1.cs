23285869dac7955e9eb022d948b0a2bfc4e69685
msharma9d <maaneksharma@gmail.com>
2024-11-03 20:07:51 -0500

    Removing failed unit tests

--- UnitTestLoginPage/UnitTest1.cs
+++ UnitTestLoginPage/UnitTest1.cs
@@ -3,7 +3,6 @@ using System;
 using System.Data;
 using System.Windows.Controls;
 
-
 namespace BookStoreLIB
 {
     [TestClass]
@@ -181,124 +180,6 @@ namespace BookStoreLIB
             inputPassword = "rz123%"; // None alphanum character
             actualReturn = userData.SignUp(inputName, inputPassword, fullName);
             Assert.AreEqual(expectedReturm, actualReturn);
-        }
-
-
-        [TestClass]
-        public class MainWindowTests
-        {
-            private MainWindowTests mainWindow;
-            private DataGrid ProductsDataGrid;
-            private UserData userData;
-            private BookOrder bookOrder;
-
-            [TestInitialize]
-            public void Setup()
-            {
-                mainWindow = new MainWindowTests();
-                userData = new UserData();
-                bookOrder = new BookOrder();
-            }
-
-            [TestMethod]
-            public void AddBook_WithoutLogin_ShouldNotAddBook()
-            {
-                // Arrange
-                userData.LogOut();
-                mainWindow.ProductsDataGrid = new DataGrid();
-                mainWindow.userData = userData;
-
-                // Act
-                mainWindow.addButton_Click(null, null);
-
-                // Assert
-                Assert.AreEqual(0, bookOrder.OrderItemList.Count);
-            }
-
-            private void addButton_Click(object value1, object value2)
-            {
-                throw new NotImplementedException();
-            }
-
-            [TestMethod]
-            public void AddBook_WithLogin_ShouldAddBook()
-            {
-                // Arrange
-                userData.LogIn("testuser", "testpassword");
-                mainWindow.userData = userData;
-                mainWindow.ProductsDataGrid = new DataGrid();
-                var row = new DataGridRow();
-                // Mock the row and add data accordingly for the book to be selected
-                mainWindow.ProductsDataGrid.Items.Add(row);
-                mainWindow.ProductsDataGrid.SelectedItem = row;
-
-                // Act
-                mainWindow.addButton_Click(null, null);
-
-                // Assert
-                Assert.AreEqual(1, bookOrder.OrderItemList.Count);
-            }
-
-            [TestMethod]
-            public void AddDuplicateBook_ShouldNotAddBookTwice()
-            {
-                // Arrange
-                userData.LogIn("testuser", "testpassword");
-                mainWindow.userData = userData;
-                mainWindow.ProductsDataGrid = new DataGrid();
-                var row = new DataGridRow();
-                mainWindow.ProductsDataGrid.Items.Add(row);
-                mainWindow.ProductsDataGrid.SelectedItem = row;
-                mainWindow.addButton_Click(null, null);
-
-                // Act
-                mainWindow.addButton_Click(null, null);
-
-                // Assert
-                Assert.AreEqual(1, bookOrder.OrderItemList.Count);
-            }
-
-            [TestMethod]
-            public void AddBookToWatchlist_WithoutLogin_ShouldNotAdd()
-            {
-                // Arrange
-                userData.LogOut();
-                mainWindow.userData = userData;
-
-                // Act
-                mainWindow.AddToWatchlist_Click(null, null);
-
-                // Assert
-                // Assert that no watchlist entries were added
-                var watchlist = new DALUserProfile().GetUserWatchlist(userData.UserID);
-                Assert.AreEqual(0, watchlist.Count);
-            }
-
-            [TestMethod]
-            public void AddBookToWatchlist_WithLogin_ShouldAdd()
-            {
-                // Arrange
-                userData.LogIn("testuser", "testpassword");
-                mainWindow.userData = userData;
-                mainWindow.ProductsDataGrid = new DataGrid();
-                var row = new DataGridRow();
-                // Mock the row and add data accordingly for the book to be selected
-                mainWindow.ProductsDataGrid.Items.Add(row);
-                mainWindow.ProductsDataGrid.SelectedItem = row;
-
-                // Act
-                mainWindow.AddToWatchlist_Click(null, null);
-
-                // Assert
-                // Assert that the watchlist contains 1 entry
-                var watchlist = new DALUserProfile().GetUserWatchlist(userData.UserID);
-                Assert.AreEqual(1, watchlist.Count);
-            }
-
-            private void AddToWatchlist_Click(object value1, object value2)
-            {
-                throw new NotImplementedException();
-            }
         }
     }
 
