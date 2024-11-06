6f7ee8e55f3f93ca62642e038944a85dd5221ce0
MoussaMJS <156938436+MoussaMJS@users.noreply.github.com>
2024-11-06 14:27:13 -0500

    Add files via upload
    New user settings xaml and xaml.cs

---
+++ rzBookstore/userStngs.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace BookStoreGUI
+{
+    public partial class userStngs : Window
+    {
+        public userStngs()
+        {
+            InitializeComponent();
+        }
+
+        private void balanceButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void updatePassButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteAccButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
