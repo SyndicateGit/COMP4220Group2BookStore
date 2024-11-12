4a16ca5d52cb85e0905ff9629f709f4784656301
MoussaMJS <156938436+MoussaMJS@users.noreply.github.com>
2024-11-12 12:41:30 -0500

    Update userStngs.xaml.cs

--- rzBookstore/userStngs.xaml.cs
+++ rzBookstore/userStngs.xaml.cs
@@ -1,13 +1,48 @@
-﻿using System;
+using System;
 using System.Windows;
+using System.ComponentModel;
+using BookStoreLIB;
 
 namespace BookStoreGUI
 {
-    public partial class userStngs : Window
+    public partial class userStngs : Window, INotifyPropertyChanged
     {
-        public userStngs()
+        private string loggedInUserName;
+        UserData userdata = new UserData();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string LoggedInUserName
+        {
+            get => loggedInUserName;
+
+            set
+            {
+                if (loggedInUserName != value)
+                {
+                    loggedInUserName = value;
+                    OnPropertyChanged(nameof(LoggedInUserName));
+                }
+            }
+        }
+        public userStngs(int userId)
         {
             InitializeComponent();
+            DataContext = this;
+            getUserName(userId);
+           
+        }
+
+        private void getUserName(int userId)
+        {
+            userProfile profile = new userProfile();
+            var dataRow = profile.GetUserProfile(userId);
+
+            LoggedInUserName = dataRow["FullName"].ToString();
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void balanceButton_Click(object sender, RoutedEventArgs e)
@@ -23,6 +58,11 @@ namespace BookStoreGUI
         private void deleteAccButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
         }
     }
 }
