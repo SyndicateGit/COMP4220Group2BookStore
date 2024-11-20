df64a20afdc312ed4ce4052091633c87e2a0c946
Moussa Jaafar-Sannan <jaafarm@uwindsor.ca>
2024-11-20 16:18:01 -0500

    lg

---
+++ UnitTestLoginPage/BookQuotes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    public class BookQuotes
+    {
+        public DataSet getBookQuotes()
+        {
+            DALBookQuotes bookQuotes = new DALBookQuotes();
+            return bookQuotes.getQuotes();
+        }
+    }
+}
---
+++ UnitTestLoginPage/DALBookQuotes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    public class DALBookQuotes
+    {
+
+        DataSet dsQuotes;
+        SqlConnection conn;
+
+        public DALBookQuotes()
+        {
+            conn = new SqlConnection(Properties.Settings.Default.MSSQLConnection);
+        }
+
+        public DataSet getQuotes()
+        {
+            try {
+                String SQL = @"SELECT Quote_id, Book_Title, Book_Author, Quote
+                                FROM BookQuotes";
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                dsQuotes = new DataSet();
+                da.Fill(dsQuotes, "BookQuotes");
+
+                return dsQuotes;
+             }
+            catch 
+            { 
+                return null;
+            }
+            
+        }
+    }
+
+    
+
+}
